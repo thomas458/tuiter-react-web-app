070bffc14bc65acae494bdf16da993da93ab0c85
Tom Hao <tomhao@Toms-MacBook-Pro.local>
2023-06-12 21:07:02 -0700

    changed back to renderJS

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // const TUITS_API = 'http://localhost:4000/api/tuits';
-const TUITS_API = 'https://tuiter-node-server-app-cnvj.onrender.com/api/tuits'
+const TUITS_API = 'https://tuiter-node-server-app-cnvj.onrender.com/api/tuits';
 // const API_BASE = process.env.REACT_APP_API_BASE;
 // const TUITS_API = `${API_BASE}/tuits`;
 
